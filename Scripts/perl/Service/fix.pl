138808a8d5038b76e1f9c80775a0131afda828cd
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2012-11-04 21:47:54 +0100

    URIs fixed to symbolicdata.org

--- Scripts/perl/Service/fix.pl
+++ Scripts/perl/Service/fix.pl
@@ -1,46 +1,21 @@
 ##################################################
 #
 # author: graebe
-# createdAt: 2006-03-04
+# createdAt: 2012-11-04
 
-# purpose: fix entries in an XML tree
-
-my $SD_HOME = $ENV{'SD_HOME'};
-die "Environment variable SD_HOME not set" unless $SD_HOME;
-use lib "$ENV{'SD_HOME'}/Scripts/perl";
+# purpose: extract entries in an XML tree
 
 use XML::DOM;
-use XML;
 use strict;
 
 my $parser=new XML::DOM::Parser;
-
+my $hash;
 map action($_), @ARGV;
+map print("$_\n"), (sort keys %$hash);
 
 sub action {
   my $fn=shift;
   my $doc=$parser->parsefile($fn) or die;
-
-  map {
-    XML::saveOWLFile($doc) if fix1($doc,$_); 
-  } $doc->getElementsByTagName("Comment");
-
+  my $name=$doc->getDocumentElement->getAttribute("createdBy");
+  $hash->{$name}=1;
 }
-
-sub fix1 {
-  my ($doc,$node)=@_;
-  return unless XML::getValue($node);
-  my $u=XML::createNode($doc,"<Comment/>");
-  $node->getParentNode->replaceChild($u,$node);
-}
-
-sub fix {
-  my $node=shift;
-  my $r=$node->getAttribute("resource");
-  $r=~s/sdxml://;
-  $r="sdxml:INTPS/$r.xml";
-  $node->removeAttribute("resource");
-  $node->setAttribute("url",$r);
-  return 1;
-}
-
